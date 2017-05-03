3a964266c9f2be8eca07f792569eb08192f81a53
umatbro <umatbroo@gmail.com>
2017-05-03 23:57:34 +0200

    Cleared Main class from comments and imports

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,8 @@
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-    //	System.out.println("Podaj odpowiedni link: ");
         WindowInterface ShowWindow = new WindowInterface();
         ShowWindow.OpenWindow();
-    //	Scanner scanner = new Scanner(System.in);
-    //	String html = scanner.nextLine();
-    //	scanner.close();
-
-    	//String html = "http://rozklady.mpk.krakow.pl/?lang=PL&rozklad=20170429&linia=151";
-//        MPKinfo busCount = new MPKinfo(html);
-//
-//        System.out.println(busCount);
-//        System.out.println("Dodatkowe informacje: " + busCount.getAdditionalInfo());
-//
-//        System.out.println("Lista weekday:\n"+busCount.getWeekdayList());
-//        System.out.println("Lista Saturday:\n"+busCount.getSaturdayList());
-//        System.out.println("Lista Sunday:\n"+busCount.getSundayList());
-//        System.out.println(busCount.getRawResult(html));
-       // MPKList mpkList = new MPKList(html);
-       // System.out.print(mpkList);
-
     }
 }
