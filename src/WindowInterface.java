a8e09056c32330cddd78c5b18d5b88cf313c9a27
MIachaI <mprawda@wp.pl>
2017-05-05 15:04:09 +0200

    Window Interface layout changed from Swing to JavaFX

--- src/WindowInterface.java
+++ src/WindowInterface.java
@@ -1,181 +1,132 @@
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import javafx.geometry.Insets;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
-public class WindowInterface extends JFrame implements ActionListener{
-
-    public JButton Exit_Button, Execute_Button, PathChoose_Button;
-    public JLabel Status_Label, Link_Label, RadioButton_Label;
-    public JTextField Link_TextField, Path_TextField;
-    public JRadioButton Cracow_RadioButton, Warsaw_RadioButton;
-    public ButtonGroup RadioPanel;
-
-    public WindowInterface() throws IOException {
-
-        //Frame initialization
-        setSize(850,250);
-        setTitle("MPK Crawler");
-        setLayout(null);
-        setResizable(false);
+import java.lang.reflect.Executable;
 
 
-        //Buttons
-        //Implementation of Execute_Button
-        Execute_Button = new JButton("Wykonaj");
-        Execute_Button.setBounds(600,200,150,20);
-        add(Execute_Button);
-        Execute_Button.addActionListener( new ActionListener() {
-            @Override
-            public void actionPerformed( ActionEvent aActionEvent ) {
-                if ( aActionEvent.getSource() == Execute_Button ){
-                    Status_Label.setText("Przetwarzam...");
-                    String html = Link_TextField.getText();
-                    MPKList mpkList = null;
-                    try {
-                        mpkList = new MPKList(html);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+public class WindowInterface extends Application implements EventHandler<ActionEvent> {
+    Button Execute_Button;
+    Stage window;
 
-                    System.out.print(mpkList);
-                    String zmienna = Path_TextField.getText();
-                    //
-                    try {
-                        File file = new File(zmienna+"/output.txt");
-                        FileWriter fileWriter = new FileWriter(file);
-                        fileWriter.write(mpkList.excelFormattedText());
-                        fileWriter.flush();
-                        fileWriter.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    //
-                    Status_Label.setText("Gotowe!");
-                }
-            }
-        } );
-
-/*        //Implementation of Exit_Button
-        Exit_Button = new JButton("Wyjście");
-        Exit_Button.setBounds(50,150,150,20);
-        add(Exit_Button);
-        Exit_Button.addActionListener( new ActionListener() {
-            @Override
-            public void actionPerformed( ActionEvent aActionEvent ) {
-                if ( aActionEvent.getSource() == Exit_Button ){
-                    dispose();
-                }
-            }
-        } );*/
-
-        //Implementation of PathChoose_Button
-        PathChoose_Button = new JButton("Wybierz ścieżkę zapisu");
-        PathChoose_Button.setBounds(10,100,200,20);
-        add(PathChoose_Button);
-        PathChoose_Button.addActionListener( new ActionListener() {
-            @Override
-            public void actionPerformed( ActionEvent aActionEvent ) {
-                if ( aActionEvent.getSource() == PathChoose_Button ){
-                    JFileChooser chooser = new JFileChooser();
-                    chooser.setCurrentDirectory(new java.io.File("."));
-                    chooser.setDialogTitle("Wybierz ścieżkę");
-                    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                    chooser.setAcceptAllFileFilterUsed(false);
-
-                    if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-                        System.out.println("getCurrentDirectory(): " + chooser.getCurrentDirectory());
-                        System.out.println("getSelectedFile() : " + chooser.getSelectedFile());
-                        Path_TextField.setText(chooser.getSelectedFile().getAbsolutePath());
-
-                    } else {
-                        System.out.println("No Selection ");
-                    }
-                }
-            }
-        } );
-
-
-        //Textfields
-        //Implementation of Link_TextField
-        Link_TextField = new JTextField("");
-        Link_TextField.setBounds(10,50, 800, 20);
-        add(Link_TextField);
-        Link_TextField.addActionListener(this);
-        Link_TextField.addActionListener( new ActionListener() {
-            @Override
-            public void actionPerformed( ActionEvent aActionEvent ) {
-                if ( aActionEvent.getSource() == Link_TextField){
-                    String html = Link_TextField.getText();
-                    MPKList mpkList = null;
-                    try {
-                        mpkList = new MPKList(html);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    System.out.print(mpkList);
-                }
-            }
-        } );
-
-        //Implementation of Path_TextField
-        Path_TextField = new JTextField("");
-        Path_TextField.setBounds(10,150,800,20);
-        add(Path_TextField);
-        Path_TextField.addActionListener(this);
-
-
-        //Labels
-        //Implementation of Link_Label
-        JLabel Link_Label = new JLabel("Wprowadź odpowiedni link:");
-        Link_Label.setBounds(10,20,200,20);
-        add(Link_Label);
-
-        //Implementation of RadioButton_Label
-        JLabel RadioButton_Label = new JLabel("Wybierz miasto:");
-        RadioButton_Label.setBounds(380,20,200,20);
-        add(RadioButton_Label);
-
-        //Implementation of Status_Label
-        Status_Label = new JLabel("Status:");
-        Status_Label.setBounds(150,150,150,20);
-        add(Status_Label);
-
-
-        //Radio Buttons
-        //Implementation of Cracow_RadioButton
-        RadioPanel = new ButtonGroup();
-        Cracow_RadioButton = new JRadioButton("Kraków");
-        Cracow_RadioButton.setBounds(500,20,80,20);
-        RadioPanel.add(Cracow_RadioButton);
-        Cracow_RadioButton.setSelected(true);
-        add(Cracow_RadioButton);
-
-        //Implementation of Warsaw_RadioButton
-        Warsaw_RadioButton = new JRadioButton("Warszawa");
-        Warsaw_RadioButton.setBounds(580,20,100,20);
-        RadioPanel.add(Warsaw_RadioButton);
-        add(Warsaw_RadioButton);
+    public static void OpenWindow() {
+        launch();
 
     }
-    public static void OpenWindow() throws IOException {
-        WindowInterface window = new WindowInterface();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setVisible(true);
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("MPK Crawler");
+
+        //GridPane with 10px padding around edge
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(8);
+        grid.setHgap(10);
+
+        ToggleGroup Location_Group_RadioButtons = new ToggleGroup();
+
+        RadioButton Cracow_Box = new RadioButton("Kraków");
+        GridPane.setConstraints(Cracow_Box, 2, 0);
+        Cracow_Box.setToggleGroup(Location_Group_RadioButtons);
+        Cracow_Box.setSelected(true);
+
+        RadioButton Warsaw_Box = new RadioButton("Warszawa");
+        GridPane.setConstraints(Warsaw_Box, 3, 0);
+        Warsaw_Box.setToggleGroup(Location_Group_RadioButtons);
+
+
+
+
+        //Link_Label - constrains use (child, column, row)
+        Label Link_Label = new Label("Wprowadź link:");
+        GridPane.setConstraints(Link_Label, 0, 0);
+
+        //Link_TextField
+        TextField Link_TextField = new TextField("");
+        GridPane.setConstraints(Link_TextField, 1, 0);
+
+        //Path_Label
+        Label Path_Label = new Label("Podaj ścieżkę zapisu:");
+        GridPane.setConstraints(Path_Label, 0, 1);
+
+        //Path_TextField
+        TextField Path_TextField = new TextField("");
+        GridPane.setConstraints(Path_TextField, 1, 1);
+        final String[] html = new String[1];
+
+        //Execute_Button
+        Button Execute_Button = new Button("Wykonaj");
+        GridPane.setConstraints(Execute_Button, 1, 2);
+        Execute_Button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String html = Link_TextField.getText();
+                MPKList mpkList = null;
+                try {
+                    mpkList = new MPKList(html);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String zmienna = Path_TextField.getText();
+                //
+                try {
+                    File file = new File(zmienna+"/output.txt");
+                    FileWriter fileWriter = new FileWriter(file);
+                    fileWriter.write(mpkList.excelFormattedText());
+                    fileWriter.flush();
+                    fileWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
+        final Button Browse_Button = new Button("...");
+        GridPane.setConstraints(Browse_Button,2,1);
+        Browse_Button.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(final ActionEvent e) {
+                        final DirectoryChooser directoryChooser =
+                                new DirectoryChooser();
+                        final File selectedDirectory =
+                                directoryChooser.showDialog(window);
+                        if (selectedDirectory != null) {
+                            selectedDirectory.getAbsolutePath();
+                            Path_TextField.setPromptText(selectedDirectory.getAbsolutePath());
+                        }
+                    }
+                }
+        );
+
+
+        //Add everything to grid
+        grid.getChildren().addAll(Link_Label, Link_TextField, Path_Label, Path_TextField, Execute_Button, Browse_Button, Cracow_Box, Warsaw_Box);
+
+
+
+        Scene scene = new Scene(grid, 600, 200);
+        window.setScene(scene);
+        window.show();
 
     }
-    public void actionPerformed(ActionEvent e)
-    {
-        Object source = e.getSource();
-        if (source==Exit_Button){
-            dispose();
-        }
-        else if (source==Execute_Button){
-            System.out.println("Przetwarzam...");
-       }
 
+    @Override
+    public void handle(ActionEvent event) {
     }
 }
