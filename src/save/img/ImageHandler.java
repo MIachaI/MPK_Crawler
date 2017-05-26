5b6593edcdfbd71abc3b957769ce3a9fb8b025a8
MIachaI <mprawda@wp.pl>
2017-05-26 11:57:41 +0200

    Output images names changed

--- src/save/img/ImageHandler.java
+++ src/save/img/ImageHandler.java
@@ -10,11 +10,13 @@ import java.io.IOException;
  */
 public class ImageHandler {
     public static void saveAllImages(ListContainer listContainer, String path) throws IOException {
+       String clearedPath = path.substring(0,path.length()-4);
+        System.out.println(clearedPath);
         for(BusInfo tram : listContainer.getOnlyTrams()){
-            HtmlToImage.imageGenerator(tram.getRawHtml(), path + tram.getLineNumberString());
+            HtmlToImage.imageGenerator(tram.getRawHtml(), clearedPath +" linia "+ tram.getLineNumberString());
         }
         for(BusInfo bus : listContainer.getOnlyBuses()){
-            HtmlToImage.imageGenerator(bus.getRawHtml(), path + bus.getLineNumberString());
+            HtmlToImage.imageGenerator(bus.getRawHtml(), clearedPath +" linia "+ bus.getLineNumberString());
         }
     }
 }
