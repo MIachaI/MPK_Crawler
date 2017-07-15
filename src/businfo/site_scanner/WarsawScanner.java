c1ece6e5e8f82ddc6285127286fbe40c9cffce86
MIachaI <mprawda@wp.pl>
2017-07-15 23:47:48 +0200

    Warsaw Scanner added

---
+++ src/businfo/site_scanner/WarsawScanner.java
@@ -0,0 +1,58 @@
+package businfo.site_scanner;
+
+import businfo.busstop.lines.LineOnStop;
+import businfo.busstop.streets.BusStop;
+import businfo.lists.BusStopLink;
+import businfo.lists.BusStopList;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by MIachaI on 15.07.17.
+ */
+public class WarsawScanner extends SiteScanner {
+    public WarsawScanner(){
+        super();
+        this.mainPageURL = "http://www.ztm.waw.pl/rozklad_nowy.php?c=183&l=1&a=2145";
+    }
+    @Override
+    public ArrayList<BusStop> scan() throws IOException {
+        ArrayList<BusStop> result = new ArrayList<>();
+        ArrayList<BusStopLink> busStopLinks;
+        busStopLinks = BusStopList.ZTMBusStopLinksGetter();
+        for(BusStopLink busStopLink : busStopLinks){
+            BusStop busStop = new BusStop(busStopLink.getName());
+
+            // now connect to a sub-site and fetch all bus-line-no:link pairs
+            String subSite = busStopLink.getLink();
+            Document document = Jsoup.connect(subSite).get();
+
+
+            // get links and numbers elements
+            Elements links = document.select("div[class=' PrzystanekLineList'] a[href]");
+            Elements numbers = document.select("div[class='PrzystanekLineList'] a[href]");
+            Element link = document.select("div[class=' PrzystanekLineList'] a[href]").first();
+            String wyswietl = link.attr("href");
+
+
+            for(Element item : links){
+                String lineLink = item.attr("href");
+                String lineNumber = item.text();
+                busStop.addBusLine(new LineOnStop(lineLink,lineNumber));
+                System.out.println(lineLink);
+                System.out.println(lineNumber);
+
+            }
+
+            // when bus stop ready add to result
+            result.add(busStop);
+        }
+
+        return result;
+    }
+}
