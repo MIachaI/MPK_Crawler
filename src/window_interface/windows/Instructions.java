22d7e96292e7c066e4fb50820b0c75218d6f81cd
umatbro <umatbroo@gmail.com>
2017-08-16 22:08:49 +0200

    prep for migrating gui to fxml

--- src/window_interface/windows/Instructions.java
+++ src/window_interface/windows/Instructions.java
@@ -1,10 +1,14 @@
 package window_interface.windows;
 
+import javafx.scene.control.TreeView;
 import javafx.stage.Stage;
 
 public class Instructions {
+    public final String STYLESHEET_DIR = "file:src/window_interface/css/Theme.css";
+
     public static Stage init(){
         Stage stage = new Stage();
+        TreeView tree = new TreeView();
 
         return stage;
     }
